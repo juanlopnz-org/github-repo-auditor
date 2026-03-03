7655adf5cba796c7b1a50aa8984b62ccdcd0d1d9
juanlopnz <jucalora17@gmail.com>
2026-03-03 04:03:17 -0500

    feat: implement HTML report generation functionality.

--- scripts/lib/reporters/html.js
+++ scripts/lib/reporters/html.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const dashboardPath = "../../../dashboard";
+const dashboardPath = "./dashboard";
 
 export function writeHtmlReport(outputPath) {
 
